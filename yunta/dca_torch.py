11241eb548ae6346004911e8a5968a71c4b3b861
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-04-09 23:11:20 +0100

    Tidying up

--- yunta/dca_torch.py
+++ yunta/dca_torch.py
@@ -13,28 +13,46 @@ import torch.nn.functional as F
 
 from .structs.msa import MSA, _A3M_ALPHABET
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def _torch_cov(x: Tensor, w: Optional[Tensor] = None) -> FloatTensor:
+def _torch_cov(
+    x: Tensor, 
+    w: Optional[Tensor] = None
+) -> FloatTensor:
     if w is None:
         return torch.cov(x)
     else:
         num_points = torch.sum(w) - torch.sqrt(torch.mean(w))
-        x_mean = torch.sum(x * w.unsqueeze(-1), 
-                           dim=0, keepdim=True) / num_points
+        x_mean = torch.sum(
+            x * w.unsqueeze(-1), 
+            dim=0, keepdim=True
+        ) / num_points
         x = (x - x_mean) * torch.sqrt(w.unsqueeze(-1))
         return torch.matmul(x.transpose(-2, -1), x) / num_points
 
 
-def _get_wip(x: Tensor, apc: bool = False, gpu: bool = True, dtype=torch.float16) -> FloatTensor:
+def _get_wip(
+    x: Tensor, 
+    apc: bool = False, 
+    gpu: bool = True, 
+    dtype=torch.float16
+) -> FloatTensor:
     device = torch.device("cuda" if( torch.cuda.is_available() and gpu) else "cpu")
     # x = x.to(device)
     alphabet_size = torch.tensor(len(_A3M_ALPHABET), device=device)
     n_row, n_col = x.shape
-    msa_one_hot = (F.one_hot(x.to(torch.int64), 
-                             num_classes=alphabet_size)
-                    .to(dtype))  # nrow, ncol, alphabet_size
-    identity_counts = torch.tensordot(msa_one_hot, msa_one_hot, 
-                                      dims=[[1,2], [1,2]])  # nrow, nrow
+    msa_one_hot = (
+        F.one_hot(
+            x.to(torch.int64), 
+            num_classes=alphabet_size,
+        )
+        .to(dtype)
+    )  # nrow, ncol, alphabet_size
+    identity_counts = torch.tensordot(
+        msa_one_hot,
+        msa_one_hot, 
+        dims=[[1,2], [1,2]],
+    )  # nrow, nrow
     # # Should be ~alphabet_size-fold fewer calculations and lower memory
     # # but actually appears to be slower?
     # identity_counts = (torch.eq(x, x)
@@ -58,40 +76,38 @@ def _get_wip(x: Tensor, apc: bool = False, gpu: bool = True, dtype=torch.float16
                                              dim=(1, 3))) * (1. - I_ncol)
 
     if apc:
-        apc_factor = (torch.sum(interchain_scores, 
-                                dim=0, keepdim=True)
-                      * torch.sum(interchain_scores, 
-                                  dim=1, keepdim=True) 
-                      / torch.sum(interchain_scores))
+        apc_factor = (
+            torch.sum(
+                interchain_scores, 
+                dim=0, keepdim=True
+            )
+            * torch.sum(
+                interchain_scores, 
+                dim=1, keepdim=True,
+            ) 
+            / torch.sum(interchain_scores))
         interchain_scores = (interchain_scores - apc_factor) * (1. - I_ncol)
 
     return interchain_scores
 
 
-def calculate_dca(msa: MSA, 
-                  apc: bool = False,
-                  gpu: bool = True) -> np.ndarray:
+def calculate_dca(
+    msa: MSA, 
+    apc: bool = False,
+    gpu: bool = True,
+) -> np.ndarray:
 
     """
     
     """
-    n_gpu = torch.cuda.device_count()
-    print_err(f"GPUs available: {n_gpu}")
-    if gpu:
-        if n_gpu == 0:
-            print_err("WARNING! GPU requested but none available. Falling back to CPU.")
-            device = 'cpu'
-        else:
-            device = 'cuda'
-            # torch.cuda.empty_cache()
-    else:
-        device = 'cpu'
-    msa_token_ids = torch.tensor(msa.sequence_token_ids,
-                                 dtype=torch.int64,
-                                 device=torch.device(device))
+    msa_token_ids = torch.tensor(
+        msa.sequence_token_ids,
+        dtype=torch.int64,
+        device=DEVICE,
+    )
     
     try:
-        wip = _get_wip(msa_token_ids, apc=apc, gpu=device == 'cuda')
+        wip = _get_wip(msa_token_ids, apc=apc, gpu=torch.cuda.is_available())
     except torch.cuda.OutOfMemoryError as e:
         print_err("GPU memory exhausted; falling back to CPU.")
         wip = _get_wip(msa_token_ids.to('cpu'), apc=apc, gpu=False)
--- yunta/plots.py
+++ yunta/plots.py
@@ -4,19 +4,21 @@ from typing import Optional
 
 import os
 
-from carabiner import colorblind_palette, print_err
-from carabiner.mpl import grid
+from carabiner import print_err
+from carabiner.mpl import colorblind_palette, figsaver, grid
 from pandas import DataFrame
 import numpy as np
 
-def plot_matrix(m: np.ndarray, 
-                filename_prefix: Optional[str] = None,
-                format: str = 'png',
-                dpi: int = 300, 
-                vline: Optional[float] = None, 
-                hline: Optional[float] = None,
-                vmax: Optional[float] = None,
-                *args, **kwargs):
+def plot_matrix(
+    m: np.ndarray, 
+    filename_prefix: Optional[str] = None,
+    format: str = 'png',
+    dpi: int = 300, 
+    vline: Optional[float] = None, 
+    hline: Optional[float] = None,
+    vmax: Optional[float] = None,
+    *args, **kwargs
+):
     
     fig, axes = grid()
     im = axes.imshow(m, cmap='magma', vmin=0., vmax=vmax)
@@ -28,26 +30,28 @@ def plot_matrix(m: np.ndarray,
     axes.set(*args, **kwargs)
 
     if filename_prefix is not None:
-        data_file = f"{filename_prefix}.tsv"
-        plot_dir = os.path.dirname(data_file)
+        plot_dir = os.path.dirname(filename_prefix)
         if not os.path.exists(plot_dir):
             print_err(f"Creating output directory {plot_dir}")
             os.makedirs(plot_dir)
-        print_err(f"Saving matrix data as {data_file}")
-        (DataFrame(m, 
-                   index=np.arange(m.shape[0]), 
-                   columns=np.arange(m.shape[1]))
-         .to_csv(data_file, sep='\t', index=False))
-        plot_file = f"{filename_prefix}.{format}"
-        print_err(f"Saving matrix plot as {plot_file}")
-        fig.savefig(plot_file, bbox_inches="tight", dpi=dpi)
+        df = DataFrame(
+            m, 
+            index=np.arange(m.shape[0]), 
+            columns=np.arange(m.shape[1]),
+        )
+        figsaver(
+            format=format,
+            dpi=dpi,
+        )(fig, filename_prefix, df=df)
 
     return fig, axes
 
-def plot_dca(dca: np.ndarray, 
-             filename_prefix: Optional[str] = None,
-             format: str = 'png',
-             dpi: int = 300):
+def plot_dca(
+    dca: np.ndarray, 
+    filename_prefix: Optional[str] = None,
+    format: str = 'png',
+    dpi: int = 300
+):
     
     if filename_prefix is not None:
         filename_prefix += "_dca"
