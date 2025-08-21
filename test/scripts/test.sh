484997e3c686a058c790c9976d8fc3891ae8a401
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-08-21 11:56:09 +0100

    Make output dir in advance

--- test/scripts/test.sh
+++ test/scripts/test.sh
@@ -2,6 +2,12 @@
 
 set -euox pipefail
 
+FILE1=test/outputs/file1.txt
+FILE2=test/outputs/file2.txt
+mkdir -p $(dirname $FILE1)
+echo "$INPUT1" > $FILE1
+echo "${INPUTB[@]}" | tr ' ' $'\n' > $FILE2
+
 # interspecies test
 INPUT_IS1=test/inputs/crypto/Q5CPK5_CRYPI.a3m
 INPUT_IS2=test/inputs/human/EZRI_HUMAN.a3m
@@ -22,12 +28,6 @@ INPUT_INTER2=test/inputs/P9WGF1_Mtb_Mmr.a3m
 INPUT1=test/inputs/DYR_YEAST.a3m
 INPUT2=test/inputs/CAPZA_YEAST.a3m
 INPUTB=(test/inputs/CAPZA_YEAST.a3m test/inputs/WWM1_YEAST.a3m)
-
-FILE1=test/outputs/file1.txt
-FILE2=test/outputs/file2.txt
-mkdir -p $(dirname $FILE1)
-echo "$INPUT1" > $FILE1
-echo "${INPUTB[@]}" | tr ' ' $'\n' > $FILE2
 
 yunta dca-single $INPUT_INTER1 -2 $INPUT_INTER2 \
     --apc \
